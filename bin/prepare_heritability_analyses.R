70462e3828d57cac001f03e788800d62def23572
c.a.warmerdam <robert_warmerdam@hotmail.com>
2026-02-25 16:08:10 +0100

    Fixed mistake in gene column for fine-mapped variants

--- bin/prepare_heritability_analyses.R
+++ bin/prepare_heritability_analyses.R
@@ -167,8 +167,8 @@ main <- function(argv = NULL) {
 
     summary_stats <- eqtl_ds %>% filter(
       phenotype == gene_data$gene_id,
-      variant_index %in% hm3_variant_ref$variant_index) %>%
-      inner_join(hm3_variant_ref, by = "variant_index") %>% collect()
+      variant_index %in% hm3_variant_ref$variant_index) %>% collect() %>%
+      inner_join(hm3_variant_ref, by = "variant_index")
 
     print(summary_stats)
 
